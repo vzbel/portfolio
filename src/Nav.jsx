b266cf969959691da2e200b79df2c3846edc7cf3
vzbel <142253332+vzbel@users.noreply.github.com>
2025-01-08 16:30:54 -0700

    Add hamburger menu toggle

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,20 +1,42 @@
 import {MoonIcon, SunIcon, Bars3Icon} from '@heroicons/react/24/outline'
+import { useState } from 'react';
 
 function Nav(){
+    // Hamburger Menu state 
+    let [isMenuVisible, setMenuVisibility] = useState(false);
+
+    // Re-opens menu if:
+    // The user opens and closes 
+    // the menu at a mobile width (w < 640px), but then resizes to tablet or desktop sizes.
+    // Without this, the mobile hamburger menu could be opened and closed but then
+    // remain hidden at larger screen sizes
+    window.addEventListener('resize', () => {
+        if(window.innerWidth >= 640 && !isMenuVisible){
+            setMenuVisibility(true)
+        }else if(window.innerWidth < 640 && isMenuVisible){
+            // This is for when the screen is resized from desktop to mobile,
+            // the menu will close automatically
+            setMenuVisibility(false)
+        }
+    });
     return (
-        <nav className='flex justify-between max-w-7xl mx-auto sticky top-0 px-6 py-6 bg-red-100'>
+        <nav className='flex justify-between max-w-7xl mx-auto sticky top-0 px-6 py-6 bg-gray-100'>
             {/* Logo */}
             <p>GAEL</p>
 
             <div className='flex gap-6'>
                 {/* Site links */}
-                <ul className='hidden gap-6 sm:flex'>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Skills</a></li>
-                    <li><a href="">Projects</a></li>
-                    <li><a href="">Contact</a></li>
-                </ul>
+                {isMenuVisible ?
+                    <ul className='flex flex-col fixed inset-0 p-24 bg-gray-100 gap-6 sm:static sm:flex-row sm:p-0'>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Skills</a></li>
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">Contact</a></li>
+                    </ul>
+                :
+                    <></>
+                }
 
                 {/* Light / Dark mode toggle */}
                 <button className='flex gap-2'>
@@ -23,7 +45,9 @@ function Nav(){
                 </button>
 
                 {/* Mobile Menu Button */}
-                <button className='block sm:hidden'>
+                <button className='block sm:hidden z-40' onClick={()=>{
+                    setMenuVisibility(!isMenuVisible)
+                }}>
                     <Bars3Icon className='size-6'/>
                 </button>
             </div>
